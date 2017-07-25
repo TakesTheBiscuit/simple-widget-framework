241458dfa19fa0c81528b57c033904da1a49a093
Josue Montano <josuemontanoa@gmail.com>
2017-07-25 00:07:43 -0400

    Render a dummy component in the app

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,12 @@
+import { h, render } from 'preact';
+
 import stylesheet from '../css/theme.scss';
+import MyComponent from '~/views/my_component';
+
+
+export function run(options = {}) {
+  const parentEl = document.querySelector(String(options.target));
+  render((
+    <MyComponent model={options.model}/>
+  ), parentEl);
+};
